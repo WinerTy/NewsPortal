1e88124eeeeeea7e9239d8cd636baeca885fd4e7
WinerTy <luchiy.v.mire@mail.ru>
2023-12-15 16:06:23 +0700

    авторизация/регистрация

--- NewsPortal/avtorization/urls.py
+++ NewsPortal/avtorization/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from . import views
+from django.urls import path, include
+from allauth.account import views as allauth_views
+from .views import ProfileView
 
-from .views import profile_view, RegisterView
 
 urlpatterns = [
-    path('profile/', profile_view, name='profile'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('accounts/', include('allauth.urls')),
+    path('login/', allauth_views.login, name='account_login'),
+    path('logout/', allauth_views.logout, name='account_logout'),
+    path('signup/', allauth_views.signup, name='account_signup'),
+    path('password/reset/', allauth_views.password_reset, name='account_reset_password'),
+    path('password/change/', allauth_views.password_change, name='account_change_password'),
+    path('profile/', ProfileView.profile, name='profile'),
 ]
